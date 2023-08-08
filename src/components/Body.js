e39deadf7c747257f5cf18e88a52e628deac20d4
nilesh0205 <nileshpatil0205@gmail.com>
2023-08-08 19:58:39 +0530

    Search Bar

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,52 @@
+import { useState } from "react";
 import { restaurantList } from "../containts";
 import RestrauntCard from "./RestrauntCard";
+
+function filterData(searchText,restaurants){
+const filterData=restaurants.filter((restaurants)=>
+  restaurants.info.name.includes(searchText)
+  );
+  return filterData;
+}
+
 const Body=()=>{
+ 
+  const [restaurants,setRestaurants]=useState(restaurantList);  
+ 
+  const [searchText, setSearchText]=useState();
     return(
+      <>
+      <div className="search-container" >
+       <input 
+       type="Text"  
+       placeholder="Search" 
+       className="search-input"
+       value={searchText} 
+       onChange={(e)=>{
+        setSearchText(e.target.value);
+       }}
+       />
+      
+       <button
+        className="Search-btn"
+        onClick={()=>{
+          //need to filter the data
+         const data=filterData(searchText,restaurants);
+         //update the state
+         setRestaurants(data);
+
+        }}
+       >Search</button>
+      </div>
         <div class="restraunt-list">
-      { restaurantList.map((restaurant)=>{
+      { restaurants.map((restaurant)=>{
         return <RestrauntCard {...restaurant.info} key={restaurant.info.id} />
       })
 
       }
       
       </div>
+      </>
     );
 };
 
